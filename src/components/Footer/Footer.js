5f1d34ce2ca9d7351dd7e066ff7820d70584440c
shem958 <shemgikunda37@gmail.com>
2024-09-25 12:32:47 +0300

    "Updated Footer component: changed copyright text, added class to ul, updated link text, added social media links and back to top button"

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,8 +5,8 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <p>&copy; 2024 ShoeStore. All rights reserved. </p>
-        <ul>
+        <p>&copy; 2024 Shoe Haven. All rights reserved.</p>
+        <ul className="footer-links">
           <li>
             <a href="/privacy">Privacy Policy</a>
           </li>
@@ -14,11 +14,38 @@ function Footer() {
             <a href="/terms">Terms of Service</a>
           </li>
           <li>
-            <a href="/contact">Contact us</a>
+            <a href="/contact">Contact Us</a>
           </li>
         </ul>
+        <div className="social-media">
+          <a
+            href="https://facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Facebook
+          </a>
+          <a
+            href="https://twitter.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Twitter
+          </a>
+          <a
+            href="https://instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Instagram
+          </a>
+        </div>
+        <button className="back-to-top" onClick={() => window.scrollTo(0, 0)}>
+          Back to Top
+        </button>
       </div>
     </footer>
   );
 }
+
 export default Footer;
